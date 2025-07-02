692ce668b63fe1c41b4866cc84ee14e16b3536f0
shoma-seki <146906404+shoma-seki@users.noreply.github.com>
2025-07-02 15:32:34 +0900

    targetPosition追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Vector2 position;
+    Vector2 targetPosition;
 
     // Start is called before the first frame update
     void Start()
