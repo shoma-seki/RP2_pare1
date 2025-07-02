1ed1c6f25d5c608692ff191503be433b9727b8d5
shoma-seki <146906404+shoma-seki@users.noreply.github.com>
2025-07-02 17:09:41 +0900

    一旦挙動完成

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector2 position;
+    Vector3 position;
+
+    [SerializeField] Sprite pa;
+    [SerializeField] Sprite gu;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        ChangeSprite();
+    }
+
+    void Move()
+    {
         position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         position = ClampToScreen();
+        position.z = -10f;
         transform.position = position;
+    }
+
+    void ChangeSprite()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            spriteRenderer.sprite = gu;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            spriteRenderer.sprite = pa;
+        }
     }
 
     Vector2 ClampToScreen()
--- Assets/ShakerScript.cs
+++ Assets/ShakerScript.cs
@@ -4,26 +4,105 @@ using UnityEngine;
 
 public class ShakerScript : MonoBehaviour
 {
+    Player player;
+
     Vector2 position;
     Vector2 targetPosition;
 
+    Vector2 prePosition;
+    Vector2 preVelocity;
     Vector2 velocity;
-    [SerializeField] float gravity;
+    [SerializeField] Vector2 gravity;
 
     bool isMouse;
     bool isGrabed;
     bool isGrounded = true;
 
+    //スタートに戻す
+    Vector2 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = FindAnyObjectByType<Player>();
+        position = transform.position;
 
+        //スタートに戻すポジション
+        startPosition = position;
+    }
+
+    private void LateUpdate()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
+        prePosition = transform.position;
 
+        if (!isGrounded)
+        {
+            Move();
+        }
+
+        Grab();
+
+        //スタートに戻す
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = startPosition;
+            position = transform.position;
+            isGrounded = true;
+        }
+
+        preVelocity = (Vector2)transform.position - prePosition;
+    }
+
+    void Move()
+    {
+        if (isGrabed)
+        {
+            targetPosition = player.transform.position;
+            position = Vector2.Lerp(position, targetPosition, 10f * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            velocity = preVelocity;
+        }
+
+        if (!isGrabed && !isGrounded)
+        {
+            if (velocity.y < gravity.y)
+            {
+                velocity.y = gravity.y;
+            }
+
+            velocity += gravity * Time.deltaTime;
+            position += velocity;
+        }
+
+        transform.position = position;
+    }
+
+    void Grab()
+    {
+        if (isMouse)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                isGrounded = false;
+                isGrabed = true;
+
+                Debug.Log("つかんだよ");
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isGrabed = false;
+            Debug.Log("離したよ");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,6 +110,7 @@ public class ShakerScript : MonoBehaviour
         if (collision.tag == "Mouse")
         {
             isMouse = true;
+            Debug.Log("マウスオーバーだお");
         }
     }
 
