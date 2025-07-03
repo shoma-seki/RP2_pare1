d85298dda3a0519f4c262d4731e8396cf056c980
ichigo-nagatomo <k023g0008@g.neec.ac.jp>
2025-07-03 16:24:47 +0900

    横向きに対応

--- Assets/Scripts/ShakerBar.cs
+++ Assets/Scripts/ShakerBar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ShakerBar : MonoBehaviour {
     [SerializeField] GameObject shakerBar;
-    [SerializeField] private float testNum = 100;
+    [SerializeField] ShakerScript shakerScript;
 
     void Start() {
         
@@ -13,16 +13,16 @@ public class ShakerBar : MonoBehaviour {
     void Update() {
         // UIの動きを確かめる用のデバッグボタン
         if (Input.GetKeyDown(KeyCode.P)) {
-            testNum -= 10f; // テスト用に値を減らす
-            testNum = Mathf.Clamp(testNum, 0f, 100f);
-            BarUpdate();
+            
         }
+        //バーの更新（常に）
+        BarUpdate();
     }
 
     // UIを動かす処理
     private void BarUpdate() {
-        // testNum(100〜0) → scaleX(1.0〜0.0) に変換
-        float scaleX = Mathf.InverseLerp(0f, 100f, testNum); // testNumが100のとき1、0のとき0になる
+        // cocktailProgress(100〜0) → scaleX(1.0〜0.0) に変換
+        float scaleX = Mathf.InverseLerp(0f, 100f, shakerScript.cocktailProgress); // cocktailProgressが100のとき1、0のとき0になる
         Vector3 scale = shakerBar.transform.localScale;
         scale.x = scaleX;
         shakerBar.transform.localScale = scale;
