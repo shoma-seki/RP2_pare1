cc4e1e40e7a15807e19d2ef902224893c95801f4
shoma-seki <146906404+shoma-seki@users.noreply.github.com>
2025-07-04 00:25:47 +0900

    グラス途中まで

---
+++ Assets/GlassScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassScript : MonoBehaviour
+{
+    Vector2 position;
+    Vector2 scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
