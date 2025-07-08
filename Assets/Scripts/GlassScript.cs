438c79d38533b86d6bfa7be7db0dbd35746eacf3
shoma-seki <146906404+shoma-seki@users.noreply.github.com>
2025-07-08 17:15:12 +0900

    エラーを修正

--- Assets/Scripts/GlassScript.cs
+++ Assets/Scripts/GlassScript.cs
@@ -38,7 +38,6 @@ public class GlassScript : MonoBehaviour
     [SerializeField] GameObject comment_great;
     [SerializeField] GameObject comment_soso;
 
-
     // Start is called before the first frame update
     void Start()
     {
