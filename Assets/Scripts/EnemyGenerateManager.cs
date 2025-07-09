bd0b4df129739fe57a8105c893b13573464c8654
ichigo-nagatomo <k023g0008@g.neec.ac.jp>
2025-07-09 09:29:12 +0900

    tuika

--- Assets/Scripts/EnemyGenerateManager.cs
+++ Assets/Scripts/EnemyGenerateManager.cs
@@ -41,8 +41,8 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 3f);
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.2f, 1f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.2f, 3f);
                 isGenerate = false;
             }
         }
@@ -58,10 +58,10 @@ public class EnemyGenerateManager : MonoBehaviour
 
         if (level == 4) {
             if (isGenerate) {
-                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 1f, 1f);
-                SpawnEnemy(2, OjamaScript.OjamaType.Stone, new Vector2(-1, 0), 1f, 2f);
-                SpawnEnemy(3, OjamaScript.OjamaType.Stone, new Vector2(1, 0), 1f, 3f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 1f, 4f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 0.7f, 1f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Stone, new Vector2(-1, 0), 0.7f, 2f);
+                SpawnEnemy(3, OjamaScript.OjamaType.Stone, new Vector2(1, 0), 0.7f, 3f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 0.7f, 4f);
 
                 isGenerate = false;
             }
@@ -71,8 +71,17 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 1f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 2f);
+
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 4f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 5f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 11f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 12f);
+
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 14f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 15f);
 
                 isGenerate = false;
             }
@@ -82,8 +91,20 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 1f, 1f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(0, -1), 1f, 1f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 1f, 3f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(0, -1), 1f, 3f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 9f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 11f);
+
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 1f, 11f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(0, -1), 1f, 11f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 1f, 13f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(0, -1), 1f, 13f);
 
                 isGenerate = false;
             }
@@ -93,8 +114,17 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Cannon, new Vector2(-1, 0), 0.1f, 1f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Cannon, new Vector2(1, 0), 0.1f, 3f);
+
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 0.7f, 7f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 0.7f, 9f);
+
+                SpawnEnemy(0, OjamaScript.OjamaType.Cannon, new Vector2(-1, 0), 0.1f, 11f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Cannon, new Vector2(1, 0), 0.1f, 13f);
+
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 0.7f, 16f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 0.7f, 17f);
 
                 isGenerate = false;
             }
@@ -104,8 +134,19 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 1f);
                 SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Stone, new Vector2(-1, -1), 0.7f, 3f);
+
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 5f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 6f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Stone, new Vector2(1, -1), 0.7f, 7f);
+
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 10f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 10f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 13f);
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 13f);
 
                 isGenerate = false;
             }
@@ -115,8 +156,20 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
-                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 2f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 2f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 5f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 5f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 8f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 8f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 11f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 11f);
+
+                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 14f);
+                SpawnEnemy(0, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 4f, 14f);
 
                 isGenerate = false;
             }
@@ -126,8 +179,41 @@ public class EnemyGenerateManager : MonoBehaviour
         {
             if (isGenerate)
             {
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.2f, 1f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.2f, 3f);
+
                 SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 1f);
-                SpawnEnemy(1, OjamaScript.OjamaType.Poison, new Vector2(0, -1), 2f, 2f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 3f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 1f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 3f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.5f, 1f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.5f, 3f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.2f, 8f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.2f, 10f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 8f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 10f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 8f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 10f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.5f, 8f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.5f, 10f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.2f, 15f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.2f, 17f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.3f, 15f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.3f, 17f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.4f, 15f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.4f, 17f);
+
+                SpawnEnemy(3, OjamaScript.OjamaType.Cannon, new Vector2(1, 1), 0.5f, 15f);
+                SpawnEnemy(2, OjamaScript.OjamaType.Cannon, new Vector2(-1, 1), 0.5f, 17f);
 
                 isGenerate = false;
             }
